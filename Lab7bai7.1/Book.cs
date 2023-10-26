b7066e39f266457b8170fe5a71d2b548086bc7cf
MaiTheLongDraco <128013309+MaiTheLongDraco@users.noreply.github.com>
2023-10-26 21:12:58 +0700

    Add project files.

---
+++ Lab7bai7.1/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7bai7._1
+{
+    internal class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Pulisher { get; set; }
+        public int Year { get; set; }
+        public int Price { get; set; }
+        public void Add()
+        {
+            Console.WriteLine("Nhap thong tin cho sach: ");
+            Console.Write("Id: "); Id = int.Parse(Console.ReadLine());
+            Console.Write("Title: ");Title = Console.ReadLine();
+            Console.Write("Author: ");Author = Console.ReadLine();
+            Console.Write("Pulisher: "); Pulisher = Console.ReadLine();
+            Console.Write("Year: "); Year = int.Parse(Console.ReadLine());
+            Console.Write("Price: "); Price = int.Parse(Console.ReadLine());
+        }
+        public override string ToString()
+        {
+            return "Thong tin sach: " +
+                "\nId: " + Id +
+                "\nTitle: " + Title +
+                "\nAuthor: " + Author +
+                "\nPulisher: " + Pulisher +
+                "\nYear: " + Year +
+                "\nPrice: " + Price;
+        }
+    }
+}
---
+++ Lab7bai7.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using Lab7bai7._1;
+using System;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<Book> books = new List<Book>(10);
+        for(int i = 0; i < 10; i++)
+        {
+            books[i].Add();
+        }
+    }
+}
